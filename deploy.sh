b3e3299c0cf8d0c53de161cf850d9aea0f0081d7
Timofey Samsonov <tsamsonov@geogr.msu.ru>
2022-02-20 18:32:53 +0300

    finish 3rd lesson

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:tsamsonov/real-world-vue.git master:gh-pages
+git push -f git@github.com:tsamsonov/touring-vue-router-sandbox.git master:gh-pages
 cd -
