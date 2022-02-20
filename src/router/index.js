655223ee35cfe2b1f5278496c710daefad3c0084
Timofey Samsonov <tsamsonov@geogr.msu.ru>
2022-02-20 23:12:01 +0300

    finish aliases (L5)

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
     props: route => ({ page: parseInt(route.query.page) || 1 })
   },
   {
-    path: '/event/:id',
+    path: '/events/:id',
     name: 'EventLayout',
     component: EventLayout,
     props: true,
@@ -37,10 +37,20 @@ const routes = [
     ]
   },
   {
-    path: '/About',
+    path: '/event/:afterEvent(.*)',
+    redirect: to => {
+      return { path: `/events/${to.params.afterEvent}` }
+    }
+  },
+  {
+    path: '/about-us',
     name: 'About',
     component: About,
   },
+  {
+    path: '/about',
+    redirect: { name: 'About' }
+  }
 ]
 
 const router = createRouter({
